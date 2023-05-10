f99a53550ce5d2e340cc421ec4606b4256647716
BingBong <angusroymcdonald@gmail.com>
2023-05-10 11:08:00 +1000

    Refactored Spotify.js + associated function calls in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,15 @@
-import Spotify from '../utils/Spotify'
+import Spotify from '../utils/Spotify';
 import SearchBar from './SearchBar';
 import SearchResults from './SearchResults';
 import Playlist from './Playlist';
 import '../styles/App.css';
 import { useCallback, useEffect, useState } from 'react';
-import Login from '../new/Login';
-import useUserAuth from '../new/UserAuth'
-import SpotifyWebApi from 'spotify-web-api-node';
-
-
-const spotifyApi = new SpotifyWebApi({
-  clientId: '1a201c080a09493aaa3b437c6f53dbaf'
-})
+import Login from './Login';
+import useUserAuth from './UserAuth'
 
 
 function App() {
+
   
   const [searchResults, setSearchResults] = useState([]);
   const [playlistTracks, setPlaylistTracks] = useState([]);
@@ -23,31 +18,22 @@ function App() {
   const codeMatch = window.location.href.match(/code=([^&]*)/);
   let code = codeMatch ? codeMatch[1] : null
   let { accessToken } = useUserAuth(code);
-  spotifyApi.setAccessToken(accessToken)
+
+
+  useEffect(() => {
+    if (accessToken) {
+      Spotify.setAccessToken(accessToken)
+    }
+  }, [accessToken])
 
 
   // SET SEARCH RESULTS
-  const search = useCallback( async (searchQuery) => {
-    // let data = await Spotify.search(searchQuery);
-    spotifyApi.searchTracks(searchQuery)
-    .then(function(data) {
-      console.log(`Search by ${searchQuery}:`);
-      let results = data.body.tracks.items.map(track => ({
-        id: track.id,
-        name: track.name,
-        artist: track.artists[0].name,
-        album: track.album.name,
-        image: track.album.images[2].url,
-        uri: track.uri
-      }))
-      setSearchResults(results);
-      console.log(searchResults)
-    }, function(err) {
-      console.error(err);
-    });
-  }, []);
+  const search = async (searchQuery) => {
+    const results = await Spotify.searchTracks(searchQuery)
+    setSearchResults(results)
+  }
 
-
+  
   // ADD TRACK TO PLAYLIST
   const addTrack = (track) => {
     if (playlistTracks.some((playlistTrack) => playlistTrack.id === track.id)
@@ -56,45 +42,25 @@ function App() {
     }
     setPlaylistTracks((prevTracks) => [...prevTracks, track]);
   }
-
-
+  
+  
   // REMOVE TRACK FROM PLAYLIST
   const removeTrack = (track) => {
     setPlaylistTracks((prevTracks) => {
       return prevTracks.filter((currentTrack) => currentTrack.id !== track.id)
     })
   }
+  
+  
+  // SAVE PLAYLIST
+  const savePlaylist = async (playlistName, playlistTracks) => {
+    Spotify.savePlaylist(playlistName, playlistTracks)
+  }
 
 
   // CHANGE PLAYLIST NAME
   const changePlaylistName = (name) => {
     setPlaylistName(name)
-  }
-
-
-  // SAVE PLAYLIST
-  const savePlaylist = async (playlistName, playlistTracks) => {
-    let playlistId;
-    let trackIds = playlistTracks.map(item => item.uri)
-    console.log(trackIds)
-
-    // Create Playlist
-    spotifyApi.createPlaylist(playlistName, { 'description': playlistName, 'public': true })
-    .then(function(data) {
-      console.log(data)
-      console.log('Playlist ID:', playlistId)
-      console.log('Created playlist!')
-      return data.body.id
-    })
-    .then(function(playlistId) {
-      return spotifyApi.addTracksToPlaylist(playlistId, trackIds)
-    })
-    .then(function(data) {
-      console.log(data)
-    })
-    .then(function(error) {
-      console.log(error);
-    });
   }
 
 
--- src/utils/Spotify.js
+++ src/utils/Spotify.js
@@ -1,101 +1,53 @@
-import axios from 'axios';
+import SpotifyWebApi from 'spotify-web-api-node'
 
-const clientID = "1a201c080a09493aaa3b437c6f53dbaf";
-const redirectURL = "http://localhost:3000/";
-const baseURL = "https://accounts.spotify.com/authorize";
+// Initialize spotifyApi outside the function to be used across multiple functions
+const spotifyApi = new SpotifyWebApi({
+  clientId: '1a201c080a09493aaa3b437c6f53dbaf',
+});
 
-let accessToken;
-let tokenExpirationTime;
-// let code;
-
-
-const Spotify = {
-
-  getAccessToken() {
-    const currentTime = new Date().getTime();
-
-    if (accessToken && currentTime < tokenExpirationTime) {
-      return accessToken;
-    }
-
-    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
-    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
-    // code = window.location.href.match(/code=([^&]*)/)
-    
-    if (accessTokenMatch && expiresInMatch) {
-      accessToken = accessTokenMatch[1];
-      const expiresIn = Number(expiresInMatch[1]);
-      tokenExpirationTime = currentTime + expiresIn * 1000;
-
-      window.setTimeout(() => {
-      accessToken = '';
-      tokenExpirationTime = 0;
-    }, expiresIn * 1000);
-
-      window.history.pushState('Access Token', null, '/');
-      return accessToken;
-    } else {
-
-
-      const scopes = 'user-read-private user-read-email playlist-modify-public playlist-modify-private';
-      const encodedScopes = encodeURIComponent(scopes);
-
-      const accessURL = `${baseURL}?client_id=${clientID}&response_type=token&scope=${encodedScopes}&redirect_uri=${encodeURIComponent(redirectURL)}`;
-      window.location = accessURL;
-
-    }
-  },
-
-  logout() {
-    accessToken = ''
-    tokenExpirationTime = null;
-    console.log('Access Token:', accessToken)
-  },
-
-  async search(query) {
-    const accessToken = Spotify.getAccessToken();
-    
-    const { data } = await axios.get(`https://api.spotify.com/v1/search?type=track&q=${query}`, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`
-      }})
-    // console.log("Raw Tracks:", data.tracks.items)
-    return data.tracks.items.map(track => ({
-      id: track.id,
-      name: track.name,
-      artist: track.artists[0].name,
-      album: track.album.name,
-      image: track.album.images[2].url,
-      uri: track.uri
-    }));
-  },
-
-
-  async save(playlistName, playlistTracks) {
-    // const accessToken = Spotify.getAccessToken();
-
-    // Collect currentUserData and save currentUserID
-    const { data: currentUserData } = await axios.get(`https://api.spotify.com/v1/me`, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`
-    }})
-    const currentUserId = currentUserData.id;
-    console.log("Current User ID:", currentUserId)
-
-    // Save playlist
-    const { data: playlistData } = await axios.post(`https://api.spotify.com/v1/users/${currentUserId}/playlists`, {
-      "name": playlistName,
-      "description": playlistName,
-      "public": "true",
-      headers: {
-        "Authorization": `Bearer ${accessToken}`,
-        "Content-Type": "application/json"
-      },
-    })
-
-    // const currentUserID = currentUserData.id;
-    console.log("Playlist Data", playlistData)
-  }
+const setAccessToken = (token) => {
+  spotifyApi.setAccessToken(token);
 }
 
-export default Spotify;
+const searchTracks = async (searchQuery) => {
+  const data = await spotifyApi.searchTracks(searchQuery);
+  console.log(data.body)
+  return data.body.tracks.items.map(track => ({
+    id: track.id,
+    name: track.name,
+    artist: track.artists[0].name,
+    album: track.album.name,
+    image: track.album.images[2].url,
+    uri: track.uri
+  }))
+}
+
+const savePlaylist = async (playlistName, playlistTracks) => {
+  let playlistId;
+  let trackIds = playlistTracks.map(item => item.uri)
+  console.log(trackIds)
+
+  // Create Playlist
+  spotifyApi.createPlaylist(playlistName, { 'description': playlistName, 'public': true })
+  .then(function(data) {
+    console.log(data)
+    console.log('Playlist ID:', playlistId)
+    console.log('Created playlist!')
+    return data.body.id
+  })
+  .then(function(playlistId) {
+    return spotifyApi.addTracksToPlaylist(playlistId, trackIds)
+  })
+  .then(function(data) {
+    console.log(data)
+  })
+  .then(function(error) {
+    console.log(error);
+  });
+}
+
+export default {
+  setAccessToken,
+  searchTracks,
+  savePlaylist,
+};
