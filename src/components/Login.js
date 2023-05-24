66d599ef5cb2349a802ef048440f16993f724855
BingBong <angusroymcdonald@gmail.com>
2023-05-24 17:53:41 +1000

    updated redirectURL for Heroku

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,8 @@ import React from 'react'
 const scopes = 'user-read-private user-read-email playlist-modify-public playlist-modify-private';
 const encodedScopes = encodeURIComponent(scopes);
 const clientID = "1a201c080a09493aaa3b437c6f53dbaf";
-const redirectURL = "http://localhost:3000/";
+const redirectURL = "https://git.heroku.com/angus-spotify-clone.git";
+// const redirectURL = "http://localhost:3000/";
 const baseURL = "https://accounts.spotify.com/authorize";
 
 const AUTH_URL = `${baseURL}?client_id=${clientID}&response_type=code&scope=${encodedScopes}&redirect_uri=${encodeURIComponent(redirectURL)}`;
