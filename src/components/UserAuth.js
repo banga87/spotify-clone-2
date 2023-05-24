41777e74684e232bf316729299ab62c47a0b0d56
BingBong <angusroymcdonald@gmail.com>
2023-05-24 18:07:28 +1000

    Heroku adjustments.

--- src/components/UserAuth.js
+++ src/components/UserAuth.js
@@ -14,7 +14,7 @@ const useUserAuth = (code) => {
 
     const fetchData = async () => {
       try {
-        const response = await axios.post('https://git.heroku.com/angus-spotify-clone.git/login', { code })
+        const response = await axios.post('https://angus-spotify-clone.herokuapp.com/login', { code })
         // const response = await axios.post('http://localhost:3001/login', { code })
           console.log('login data:', response.data);
           setAccessToken(response.data.accessToken);
@@ -42,7 +42,7 @@ const useUserAuth = (code) => {
   
     const fetchRefreshedToken = async () => {
       try {
-        const response = await axios.post('https://git.heroku.com/angus-spotify-clone.git/refresh', {
+        const response = await axios.post('https://angus-spotify-clone.herokuapp.com/refresh', {
         // const response = await axios.post('http://localhost:3001/refresh', {
           refreshToken,
         });
