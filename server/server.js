d61fa9e761a2923768af8bfb40e7d61b11d3bff7
BingBong <angusroymcdonald@gmail.com>
2023-06-06 11:30:21 +1000

    deployment changes to listening port.

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require("cors");
 const bodyParser = require("body-parser")
 const SpotifyWebApi = require('spotify-web-api-node');
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -79,4 +80,7 @@ app.post('/login', (request, response) => {
     })
 });
 
-app.listen(3001);
+// app.listen(3001);
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
